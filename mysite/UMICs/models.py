3382f24e0c88e00246f2efcf1249bb10e56a7598
idolbyjuly <idolbyjuly@gmail.com>
2022-01-25 02:30:42 -0800

    create app for project

---
+++ mysite/UMICs/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+# Create your models here.
+
+class New_Increasing(mondels.Model):
+    date = models.DateField()
+    video_count = models.IntegerField(default=0)
+    sum_view = models.IntegerField(default=0)
+    max_view = models.FloatField(default=0.0)
+    avg_view = models.FloatField(default=0.0)
+    avg_like = models.FloatField(default=0.0)
+    avg_comment = models.FloatField(default=0.0)
+
+
+class Playlist_Type(models.Model):
+    video_id =
+    _c0 =
+    channel_id =
+    video_title =
+    video_tags =
+    video_description =
+    upload_date =
+    view_count =
+    like_count =
+    subscriber_count =
+    comment_count =
+    duration_count =
+    lofi,hiphop =
+    relax =
+    chill =
+    study =
+    jazz =
+    beat =
+    sleep =
+    meditation =
+    lounge =
+    cafe =
+    piano =
+    other =
+    URL =
+
+class Predict_Table(models.Model):
+    video_id = 
+    upload_day =
+    title = 
+    description = 
+    tags = 
+    duration =
+    subscriber_count = 
+    view22 = 
+    like_22 =
+    comment_22 =
+    view_inc_23 =
+    view_inc_24_noon =
+    view_inc_24_night =
+    view_inc_25_noon =
+    view_inc_25_night= 
+    like_inc_23 =
+    like_inc_24_noon =
+    like_inc_24_night =
+    like_inc_25_noon =
+    like_inc_25_night =
+    comment_inc_23 =
+    comment_inc_24_noon =
+    comment_inc_24_night =
+    comment_inc_25_noon =
+    comment_inc_25_night =
+    like_26,comment_26 =
+    view_26 =
+
